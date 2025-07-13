0c2e0e346f91627b99370f4ac89f0aa93c5653d9
Sanjay Baskar <sanjaybaskar.in@gmail.com>
2025-07-13 07:01:48 +0530

    Created ui of interview and added routed for requests

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,17 @@
 import React, {ReactNode} from 'react'
 import Link from "next/link";
 import Image from "next/image";
+import {redirect} from "next/navigation";
+import {isAuthenticated} from "@/lib/actions/auth.action";
 
-const RootLayout = ({children} : {children : ReactNode}) => {
+const RootLayout = async ({children} : {children : ReactNode}) => {
+    const isUserAuthenticated = await isAuthenticated();
+    if(!isUserAuthenticated) redirect('/sign-in');
+
     return (
         <div className="root-layout">
+
+
             <nav>
                 <Link href="/" className="flex items-center">
                     <Image src="/logo.svg" alt="Logo" width={38} height={32}/>
--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,1 +1,121 @@
+// since sometimes it may get to client side
+// we strictly restrict it using :
+'use server';
+
 // start authentication
+import {auth, db} from "@/Firebase/admin";
+import {cookies} from "next/headers";
+
+const ONE_WEEK = 60 * 60 * 24 * 7 * 1000;
+
+
+export async function signUp(params: SignUpParams) {
+    const { uid, name, email } = params;
+
+    try {
+        const userRecord = await db.collection('users').doc(uid).get();
+
+        if (userRecord.exists){
+            return {
+                success: false,
+                message: 'User already exists!. Please sign in instead',
+            }
+        }
+        await db.collection('users').doc(uid).set({
+            name, email
+        })
+
+        return {
+            success: true,
+            message: 'User successfully created!',
+        }
+        // for error handling is down
+    } catch (e : any){
+        console.error('Error creating a user. Try Again Later',e);
+        if(e.code === 'auth/email-already-exists') {
+            return {
+                success: false,
+                message: 'Email already exists'
+            }
+        }
+
+        return {
+            success: false,
+            message: 'Failed to create a account. Try Again Later',
+        }
+    }
+}
+
+export async function signIn(params: SignInParams) {
+    const { email, idToken } = params;
+
+    try {
+        const userRecord = await auth.getUserByEmail(email);
+
+        if(!userRecord){
+            return {
+                success: false,
+                message: 'User does not exist. Create account instead!',
+            }
+
+        }
+        
+        await setSessionCookies(idToken);
+    }catch (e: any){
+        console.log(e);
+
+        return {
+            success: false,
+            message: 'Failed to log in to account',
+        }
+    }
+}
+
+export async function setSessionCookies(idToken: string) {
+    const cookieStore = await cookies();
+
+    const sessionCookie = await auth.createSessionCookie(idToken, {
+        expiresIn : ONE_WEEK, // 60 - 60 - 24 - 7 means a week and 1000 is for milliseconds
+    })
+
+    cookieStore.set('session', sessionCookie, {
+        maxAge: ONE_WEEK,
+        httpOnly: true,
+        secure:process.env.NODE_ENV === 'production',
+        path: '/',
+        sameSite : 'lax',
+    })
+}
+
+export async function getCurrentUser(): Promise<User | null> {
+    const cookieStore = await cookies();
+    const sessionCookie = cookieStore.get('session')?.value;
+
+    if(!sessionCookie) return null;
+
+    try {
+        const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+        const userRecord = await db
+            .collection('users')
+            .doc(decodedClaims.uid)
+            .get();
+
+        if (!userRecord.exists) {
+            return null;
+        }
+
+        return {
+            ...userRecord.data(),
+            id: userRecord.id,
+        } as User;
+
+    }catch (e: any){
+        console.error(e);
+        return null;
+    }
+}
+
+export async function isAuthenticated(){
+    const user = await getCurrentUser();
+    return !!user; // helps to conver string to boolean value
+}
